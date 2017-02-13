a26cced4b7d939c77e32320de8e8bbc937319674
Boyan Bonev <boyan.i.bonev@gmail.com>
2017-02-13 18:02:25 +0000

    Renamed Main method to example one
    Renamed it to StartGameExample for whoever wants to test only the game.

--- src/teamproject/graphics/event/listener/NewGameListener.java
+++ src/teamproject/graphics/event/listener/NewGameListener.java
@@ -1,7 +1,41 @@
 package teamproject.graphics.event.listener;
 
+import teamproject.event.arguments.container.NewGameStartedEventArguments;
+import teamproject.event.listener.NewGameStartedEventListener;
+import teamproject.gamelogic.domain.Behaviour;
+import teamproject.graphics.*;
+
 /**
  * Created by boyanbonev on 13/02/2017.
  */
-public class NewGameListener {
+public class NewGameListener implements NewGameStartedEventListener {
+    @Override
+    public void onNewGameStarted(NewGameStartedEventArguments args) {
+        final Behaviour sampleBehavior = new BasicBehaviour(Behaviour.Type.DEFAULT);
+
+        final GridVisualisation grid = new GridVisualisation();
+        final MapVisualisation mapV = new MapVisualisation(grid);
+
+        // Initialize Screen dimensions
+        PositionVisualisation.initScreenDimensions();
+
+        // Generate Map
+        //args.stage.setScene(mapV.generateMap());
+        //args.stage.show();
+
+        // Add CLick Listener
+        mapV.addClickListener();
+
+        // Create Pacman
+        GamePlay.pacman = new PacmanVisualisation(sampleBehavior, "Player1", grid, mapV);
+
+        // Create Ghost
+        GamePlay.ghost1 = new GhostVisualisation(sampleBehavior, "Ghost1", grid, GamePlay.pacman, mapV);
+
+        // Redraw Map
+        mapV.redrawMap();
+
+        // Start Timeline
+        mapV.startTimeline();
+    }
 }
