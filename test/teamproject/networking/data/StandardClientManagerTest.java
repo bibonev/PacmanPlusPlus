ecfaadcd179aaf2c0d0ab8f1c497c1e78a78723d
Boyan Bonev <boyan.i.bonev@gmail.com>
2017-02-18 17:27:57 +0000

    Uncomment networking tests.

--- test/teamproject/networking/data/StandardClientManagerTest.java
+++ test/teamproject/networking/data/StandardClientManagerTest.java
@@ -9,51 +9,51 @@ import org.junit.Test;
 import teamproject.networking.StandardClientManager;
 
 public class StandardClientManagerTest {
-//	@Test
-//	public void testTriggersTriggered() {
-//		ArrayList<Integer> received = new ArrayList<Integer>();
-//
-//		StandardClientManager nm = new StandardClientManager(new MockSocket(0));
-//
-//		nm.addTrigger(t -> {
-//			received.add(t.getInteger("value"));
-//		}, "data");
-//
-//		nm.addTrigger(t -> {
-//			received.add(t.getInteger("value") + 1);
-//		}, "data-plus-1");
-//
-//		Packet p1 = new Packet("data");
-//		p1.setInteger("value", 3);
-//		nm.receive(p1.toString().getBytes(StandardCharsets.UTF_8));
-//
-//		Packet p2 = new Packet("data-plus-1");
-//		p2.setInteger("value", 5);
-//		nm.receive(p2.toString().getBytes(StandardCharsets.UTF_8));
-//
-//		assertTrue(received.contains(3));
-//		assertTrue(received.contains(6));
-//		assertEquals(2, received.size());
-//	}
-//
-//	@Test(expected=RuntimeException.class)
-//	public void testNonexistentTrigger() {
-//		StandardClientManager nm = new StandardClientManager(new MockSocket(0));
-//		Packet p1 = new Packet("data");
-//		nm.receive(p1.toString().getBytes(StandardCharsets.UTF_8));
-//	}
-//
-//	@Test(expected=IllegalArgumentException.class)
-//	public void testNoMultipleTriggers() {
-//		StandardClientManager nm = new StandardClientManager(new MockSocket(0));
-//
-//
-//		nm.addTrigger(t -> {
-//			System.out.println("d");
-//		}, "data");
-//
-//		nm.addTrigger(t -> {
-//			System.out.println("e");
-//		}, "data");
-//	}
+	@Test
+	public void testTriggersTriggered() {
+		ArrayList<Integer> received = new ArrayList<Integer>();
+
+		StandardClientManager nm = new StandardClientManager(new MockSocket(0));
+
+		nm.addTrigger(t -> {
+			received.add(t.getInteger("value"));
+		}, "data");
+
+		nm.addTrigger(t -> {
+			received.add(t.getInteger("value") + 1);
+		}, "data-plus-1");
+
+		Packet p1 = new Packet("data");
+		p1.setInteger("value", 3);
+		nm.receive(p1.toString().getBytes(StandardCharsets.UTF_8));
+
+		Packet p2 = new Packet("data-plus-1");
+		p2.setInteger("value", 5);
+		nm.receive(p2.toString().getBytes(StandardCharsets.UTF_8));
+
+		assertTrue(received.contains(3));
+		assertTrue(received.contains(6));
+		assertEquals(2, received.size());
+	}
+
+	@Test(expected=RuntimeException.class)
+	public void testNonexistentTrigger() {
+		StandardClientManager nm = new StandardClientManager(new MockSocket(0));
+		Packet p1 = new Packet("data");
+		nm.receive(p1.toString().getBytes(StandardCharsets.UTF_8));
+	}
+
+	@Test(expected=IllegalArgumentException.class)
+	public void testNoMultipleTriggers() {
+		StandardClientManager nm = new StandardClientManager(new MockSocket(0));
+
+
+		nm.addTrigger(t -> {
+			System.out.println("d");
+		}, "data");
+
+		nm.addTrigger(t -> {
+			System.out.println("e");
+		}, "data");
+	}
 }
--- test/teamproject/networking/data/StandardServerManagerTest.java
+++ test/teamproject/networking/data/StandardServerManagerTest.java
@@ -18,52 +18,52 @@ import teamproject.networking.event.ClientConnectedListener;
 import teamproject.networking.event.ClientDisconnectedListener;
 
 public class StandardServerManagerTest {
-//	@Test
-//	public void testTriggersTriggered() {
-//		HashMap<Integer, Integer> received = new HashMap<Integer, Integer>();
-//
-//		StandardServerManager nm = new StandardServerManager(new MockServer());
-//
-//		nm.addTrigger((i, t) -> {
-//			received.put(i, t.getInteger("value"));
-//		}, "data");
-//
-//		nm.addTrigger((i, t) -> {
-//			received.put(i, t.getInteger("value") + 1);
-//		}, "data-plus-1");
-//
-//		Packet p1 = new Packet("data");
-//		p1.setInteger("value", 3);
-//		nm.receive(5, p1.toString().getBytes(StandardCharsets.UTF_8));
-//
-//		Packet p2 = new Packet("data-plus-1");
-//		p2.setInteger("value", 5);
-//		nm.receive(3, p2.toString().getBytes(StandardCharsets.UTF_8));
-//
-//		assertTrue(received.containsKey(5));
-//		assertEquals(3, (int)received.get(5));
-//		assertTrue(received.containsKey(3));
-//		assertEquals(6, (int)received.get(3));
-//	}
-//
-//	@Test(expected=RuntimeException.class)
-//	public void testNonexistentTrigger() {
-//		StandardServerManager nm = new StandardServerManager(new MockServer());
-//		Packet p1 = new Packet("data");
-//		nm.receive(3, p1.toString().getBytes(StandardCharsets.UTF_8));
-//	}
-//
-//	@Test(expected=IllegalArgumentException.class)
-//	public void testNoMultipleTriggers() {
-//		StandardServerManager nm = new StandardServerManager(new MockServer());
-//
-//
-//		nm.addTrigger((i, t) -> {
-//			System.out.println("d");
-//		}, "data");
-//
-//		nm.addTrigger((i, t) -> {
-//			System.out.println("e");
-//		}, "data");
-//	}
+	@Test
+	public void testTriggersTriggered() {
+		HashMap<Integer, Integer> received = new HashMap<Integer, Integer>();
+
+		StandardServerManager nm = new StandardServerManager(new MockServer());
+
+		nm.addTrigger((i, t) -> {
+			received.put(i, t.getInteger("value"));
+		}, "data");
+
+		nm.addTrigger((i, t) -> {
+			received.put(i, t.getInteger("value") + 1);
+		}, "data-plus-1");
+
+		Packet p1 = new Packet("data");
+		p1.setInteger("value", 3);
+		nm.receive(5, p1.toString().getBytes(StandardCharsets.UTF_8));
+
+		Packet p2 = new Packet("data-plus-1");
+		p2.setInteger("value", 5);
+		nm.receive(3, p2.toString().getBytes(StandardCharsets.UTF_8));
+
+		assertTrue(received.containsKey(5));
+		assertEquals(3, (int)received.get(5));
+		assertTrue(received.containsKey(3));
+		assertEquals(6, (int)received.get(3));
+	}
+
+	@Test(expected=RuntimeException.class)
+	public void testNonexistentTrigger() {
+		StandardServerManager nm = new StandardServerManager(new MockServer());
+		Packet p1 = new Packet("data");
+		nm.receive(3, p1.toString().getBytes(StandardCharsets.UTF_8));
+	}
+
+	@Test(expected=IllegalArgumentException.class)
+	public void testNoMultipleTriggers() {
+		StandardServerManager nm = new StandardServerManager(new MockServer());
+
+
+		nm.addTrigger((i, t) -> {
+			System.out.println("d");
+		}, "data");
+
+		nm.addTrigger((i, t) -> {
+			System.out.println("e");
+		}, "data");
+	}
 }
