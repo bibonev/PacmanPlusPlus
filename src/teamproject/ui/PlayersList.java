bde0a554f3458f088a2aa906870476db23eac8d6
Andrei-Marius Longhin <axl518@cs.bham.ac.uk>
2017-03-08 14:45:32 +0000

    Add logic for multiplayer game ending

--- src/teamproject/ui/PlayersList.java
+++ src/teamproject/ui/PlayersList.java
@@ -1,53 +1,56 @@
 package teamproject.ui;
 
 import java.util.HashMap;
+
 import javafx.application.Platform;
 import javafx.scene.control.Label;
 import teamproject.gamelogic.core.LobbyPlayerInfo;
 
 /**
- * Screen to be added to the multiplayer lobby to list all current players in the game
- * 
+ * Screen to be added to the multiplayer lobby to list all current players in
+ * the game
+ *
  * @author Rose Kirtley
  *
  */
 public class PlayersList extends Screen {
 	private HashMap<Integer, Label> playerLabels;
 
-	public PlayersList(GameUI game) {
+	public PlayersList(final GameUI game) {
 		super(game);
-		
+
 		playerLabels = new HashMap<>();
-        
-        /*Collection<Player> players = game.getGame().getWorld().getPlayers();
-                
-        Iterator<Player> it = players.iterator();
-        
-        while(it.hasNext()){
-        	Label name = new Label(it.next().getName());
-        	name.setStyle(labelStyle);
-        	pane.getChildren().add(name);
-        }*/
+
+		/*
+		 * Collection<Player> players = game.getGame().getWorld().getPlayers();
+		 * 
+		 * Iterator<Player> it = players.iterator();
+		 * 
+		 * while(it.hasNext()){ Label name = new Label(it.next().getName());
+		 * name.setStyle(labelStyle); pane.getChildren().add(name); }
+		 */
 	}
-	
-	public void addPlayer(LobbyPlayerInfo player) {
+
+	public void addPlayer(final LobbyPlayerInfo player) {
 		Platform.runLater(() -> {
-			Label label = new Label(player.getName());
-	    	label.getStyleClass().add("labelStyle");
+			final Label label = new Label(player.getName());
+			label.getStyleClass().add("labelStyle");
 			pane.getChildren().add(label);
 			playerLabels.put(player.getID(), label);
 		});
 	}
 
-	public void removePlayer(int playerID) {
+	public void removePlayer(final int playerID) {
 		Platform.runLater(() -> {
-			Label label = playerLabels.remove(playerID);
-			pane.getChildren().remove(label);
+			if (playerLabels.containsKey(playerID)) {
+				final Label label = playerLabels.remove(playerID);
+				pane.getChildren().remove(label);
+			}
 		});
 	}
-	
+
 	public void clear() {
-		for(int i : playerLabels.keySet()) {
+		for (final int i : playerLabels.keySet()) {
 			removePlayer(i);
 		}
 	}
