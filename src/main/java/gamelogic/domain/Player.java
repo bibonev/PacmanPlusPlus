e6f080757b4ce6458cf3a451f5638e722917f103
Simeon Kostadinov <simeon.kostadinoff@gmail.com>
2017-03-20 16:50:07 +0000

    Added and integrated abilities - PacShield and PacLaser.

--- src/main/java/gamelogic/domain/Player.java
+++ src/main/java/gamelogic/domain/Player.java
@@ -3,16 +3,36 @@ package main.java.gamelogic.domain;
 import main.java.constants.CellState;
 import main.java.event.arguments.PlayerMovedEventArgs;
 
+/**
+ *
+ * @author Tom Galvin
+ * @author Lyubomir Pashev
+ * @author Simeon Kostadinov
+*/
 public abstract class Player extends Entity {
 	private String deathReason;
 	private String name;
 	private double angle;
 	private int dotsEaten;
 	private SkillSet skillSet;
+    private int shield;
 
 	public Player(final String name) {
 		this.name = name;
 		dotsEaten = 0;
+		this.shield = 0;
+
+		// Create SkillSet for each Player when added
+        PacShield shield = new PacShield();
+        shield.setCD(40);
+        shield.setOwner(this);
+        PacLaser laser = new PacLaser();
+        laser.setCD(20);
+        laser.setOwner(this);
+        SkillSet skillSet = new SkillSet();
+        skillSet.setQ(laser); // set E button to activate laser
+        skillSet.setW(shield); // set W button to activate shield
+        this.setSkillSet(skillSet);
 	}
 
 	/**
@@ -37,6 +57,43 @@ public abstract class Player extends Entity {
 		return dotsEaten;
 	}
 
+    /**
+     * Fetch the entity's shield
+     *
+     * @return a boolean
+     */
+    public int getShield() {
+        return this.shield;
+    }
+
+    /**
+     * Reduce the entity's shield
+     *
+     */
+    public void reduceShield() {
+        this.shield = shield - 1;
+    }
+
+    /**
+     * Update the entity's shield
+     *
+     * @param shield
+     *            the new shield value
+     */
+    public void setShield(final int shield) {
+        this.shield = shield;
+    }
+
+    public void incrementCoolDown(){
+        int coolDown = skillSet.getQ().getCD();
+        if(coolDown < 20){
+            skillSet.getQ().setCD(coolDown + 1);
+        }
+        coolDown = skillSet.getW().getCD();
+        if(coolDown < 40 && shield == 0){
+            skillSet.getW().setCD(coolDown + 1);
+        }
+    }
 	/**
 	 * Fetch the player's skillset
 	 *
